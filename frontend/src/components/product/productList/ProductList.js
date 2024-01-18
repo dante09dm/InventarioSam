0cbdc1a0527cc0ebb7533eea409575a4a52a4754
dante09dm <dante09dmm@gmail.com>
2024-01-18 11:07:19 -0300

    lanthercheck

--- frontend/src/components/product/productList/ProductList.js
+++ frontend/src/components/product/productList/ProductList.js
@@ -109,50 +109,52 @@ const ProductList = ({ products, isLoading }) => {
                   <th>Weight</th>
                   <th>Quantity</th>
                   <th>Product Weight</th>
+                  <th>Barcode</th>
                   <th>Action</th>
                 </tr>
               </thead>
 
               <tbody>
-                {currentItems.map((product, index) => {
-                  const { _id, name, category, price, quantity } = product;
-                  return (
-                    <tr key={_id}>
-                      <td>{index + 1}</td>
-                      <td>{shortenText(name, 16)}</td>
-                      <td>{category}</td>
-                      <td>
-                        {price}
-                        {" GR"}
-                      </td>
-                      <td>{quantity}</td>
-                      <td>
-                        {price * (quantity / 1000)}
-                        {" KG"}
-                      </td>
-                      <td className="icons">
-                        <span>
-                          <Link to={`/product-detail/${_id}`}>
-                            <AiOutlineEye size={25} color={"purple"} />
-                          </Link>
-                        </span>
-                        <span>
-                          <Link to={`/edit-product/${_id}`}>
-                            <FaEdit size={20} color={"green"} />
-                          </Link>
-                        </span>
-                        <span>
-                          <FaTrashAlt
-                            size={20}
-                            color={"red"}
-                            onClick={() => confirmDelete(_id)}
-                          />
-                        </span>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
+  {currentItems.map((product, index) => {
+    const { _id, name, category, price, quantity, image } = product;
+    return (
+      <tr key={_id}>
+        <td>{index + 1}</td>
+        <td>{shortenText(name, 16)}</td>
+        <td>{category}</td>
+        <td>{`${price} GR`}</td>
+        <td>{quantity}</td>
+        <td>{(price * (quantity / 1000)).toFixed(2)} KG</td>
+        <td>
+          {image && image.filePath ? (
+            <img src={image.filePath} alt={`${name} barcode`} className="barcode-image" />
+          ) : (
+            "No Barcode"
+          )}
+        </td>
+        <td className="icons">
+          <span>
+            <Link to={`/product-detail/${_id}`}>
+              <AiOutlineEye size={25} color={"purple"} />
+            </Link>
+          </span>
+          <span>
+            <Link to={`/edit-product/${_id}`}>
+              <FaEdit size={20} color={"green"} />
+            </Link>
+          </span>
+          <span>
+            <FaTrashAlt
+              size={20}
+              color={"red"}
+              onClick={() => confirmDelete(_id)}
+            />
+          </span>
+        </td>
+      </tr>
+    );
+  })}
+</tbody>
             </table>
           )}
         </div>
